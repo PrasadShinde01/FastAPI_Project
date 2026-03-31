a14e3e220ed482f37c12538e64cb6b86b59e9db4
Prasad Shinde <prasadshinde474@gmail.com>
2026-03-31 14:02:16 +0530

    init constructor practice

--- numpyPracticetest.py
+++ numpyPracticetest.py
@@ -1,7 +1,13 @@
-# N,M = map(int,input('enter the input:').split(","))
-# print(N,M)
-N,M=7,21
-# for i in range(1,N+1,1):
-#     print('-'*(N+(M//N)-1),'.'*((M//N)-i),'-'*(N+(M//N)-1))
-for i in range(2,20,2):
-    print(i,'hello',type(i))
+class Computer:
+    def __init__(self,cpu,ram):
+        self.cpu = cpu
+        self.ram = ram
+
+    def config(self):
+        print('config of this com is',self.cpu,self.ram)
+
+comp1 = Computer('i7',32)
+comp2 = Computer("Ryzon5",16)
+
+comp1.config()
+comp2.config()
