2a9105d68255049306d2283ab0537c248a51f4f6
Prasad Shinde <prasadshinde474@gmail.com>
2026-04-15 21:32:03 +0530

    parameters as required default optional

--- main.py
+++ main.py
@@ -295,5 +295,35 @@ async def updateStudent(id: int, name: str, age: int, grade: str, marks: int):
     
     return JSONResponse(content=new_student, status_code=status.HTTP_201_CREATED)
 
-# print(students)
-# print(type(students))
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/itemsOptional/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/itemsReqDefOpt/{item_id}")    #define some parameters as required, some as having a default value, and some entirely optional
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
